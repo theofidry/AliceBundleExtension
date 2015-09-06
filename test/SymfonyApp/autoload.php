cdbc310d64ce861a7c7c238eb4d6df371c50303f
Théo FIDRY <theo.fidry@gmail.com>
2015-09-06 18:12:25 +0100

    StyleCI fix

--- test/SymfonyApp/autoload.php
+++ test/SymfonyApp/autoload.php
@@ -13,10 +13,10 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 /**
-* @var ClassLoader $loader
-*/
+ * @var ClassLoader $loader
+ */
 $loader = require __DIR__.'/../../vendor/autoload.php';
 
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 return $loader;
